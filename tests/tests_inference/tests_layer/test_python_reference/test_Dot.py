68ba26852177e7bcc1793460332fc06c7b1a907f
Benjamin Lesage <m.aqpl.c@gmail.com>
2024-07-04 10:36:38 +0200

    Fix layer inference test not running

--- tests/tests_inference/tests_layer/test_python_reference/test_Dot.py
+++ tests/tests_inference/tests_layer/test_python_reference/test_Dot.py
@@ -1,35 +1,32 @@
-"""
- *******************************************************************************
- * ACETONE: Predictable programming framework for ML applications in safety-critical systems
- * Copyright (c) 2022. ONERA
- * This file is part of ACETONE
- *
- * ACETONE is free software ;
- * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation ;
- * either version 3 of  the License, or (at your option) any later version.
- *
- * ACETONE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this program ;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- ******************************************************************************
+"""*******************************************************************************
+* ACETONE: Predictable programming framework for ML applications in safety-critical systems
+* Copyright (c) 2022. ONERA
+* This file is part of ACETONE
+*
+* ACETONE is free software ;
+* you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation ;
+* either version 3 of  the License, or (at your option) any later version.
+*
+* ACETONE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this program ;
+* if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+******************************************************************************
 """
 
-acetoneTestCase_path = '/'.join(__file__.split('/')[:-3])
-import sys
-sys.path.append(acetoneTestCase_path)
-import acetoneTestCase
 import unittest
 
-import tensorflow as tf
 import keras
 import numpy as np
-from keras.layers import Input, Dense, Dot
+import tensorflow as tf
+from keras.layers import Dense, Dot, Input
 
-tf.keras.backend.set_floatx('float32')
+from tests.tests_inference import acetoneTestCase
+
+tf.keras.backend.set_floatx("float32")
 
 
 class TestDot(acetoneTestCase.AcetoneTestCase):
@@ -37,22 +34,27 @@ class TestDot(acetoneTestCase.AcetoneTestCase):
 
     @unittest.expectedFailure
     def testDot(self):
-        testshape = (1,1,16,)
+        testshape = (1, 1, 16)
         units = 8
 
         input = Input(testshape)
-        x1 = Dense(units, activation='linear', bias_initializer='he_normal')(input)
-        x2 = Dense(units, activation='linear', bias_initializer='he_normal')(input)
+        x1 = Dense(units, activation="linear", bias_initializer="he_normal")(input)
+        x2 = Dense(units, activation="linear", bias_initializer="he_normal")(input)
         out = Dot(axes=1)([x1, x2])
         model = keras.Model(inputs=[input], outputs=out)
 
-        dataset = acetoneTestCase.create_dataset(self.tmpdir_name,testshape)
-        model.save(self.tmpdir_name+'/model.h5')
+        dataset = acetoneTestCase.create_dataset(self.tmpdir_name, testshape)
+        model.save(self.tmpdir_name + "/model.h5")
 
-        acetone_result = acetoneTestCase.run_acetone_for_test(self.tmpdir_name,self.tmpdir_name+'/model.h5', self.tmpdir_name+'/dataset.txt')
+        acetone_result = acetoneTestCase.run_acetone_for_test(
+            self.tmpdir_name,
+            self.tmpdir_name + "/model.h5",
+            self.tmpdir_name + "/dataset.txt",
+        )
         keras_result = np.array(model.predict(dataset)).flatten()
 
         self.assertListAlmostEqual(list(list(acetone_result[1])), list(keras_result))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     acetoneTestCase.main()
--- tests/tests_inference/tests_layer/test_python_reference/test_Gather.py
+++ tests/tests_inference/tests_layer/test_python_reference/test_Gather.py
@@ -1,125 +1,142 @@
+"""*******************************************************************************
+* ACETONE: Predictable programming framework for ML applications in safety-critical systems
+* Copyright (c) 2022. ONERA
+* This file is part of ACETONE
+*
+* ACETONE is free software ;
+* you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation ;
+* either version 3 of  the License, or (at your option) any later version.
+*
+* ACETONE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this program ;
+* if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+******************************************************************************
 """
- *******************************************************************************
- * ACETONE: Predictable programming framework for ML applications in safety-critical systems
- * Copyright (c) 2022. ONERA
- * This file is part of ACETONE
- *
- * ACETONE is free software ;
- * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation ;
- * either version 3 of  the License, or (at your option) any later version.
- *
- * ACETONE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this program ;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- ******************************************************************************
-"""
-
-acetoneTestCase_path = '/'.join(__file__.split('/')[:-3])
-import sys
-sys.path.append(acetoneTestCase_path)
-import acetoneTestCase
 
 import numpy as np
 import onnx
 import onnxruntime as rt
 
+from tests.tests_inference import acetoneTestCase
+
+
 class TestGather(acetoneTestCase.AcetoneTestCase):
     """Test for Concatenate Layer"""
 
     def testGather1(self):
-        testshape = (1,3,10,10)
+        testshape = (1, 3, 10, 10)
 
         model_input_name = "X"
-        X = onnx.helper.make_tensor_value_info(model_input_name,
-                                            onnx.TensorProto.FLOAT,
-                                            [ None,3, 10, 10])
+        X = onnx.helper.make_tensor_value_info(
+            model_input_name,
+            onnx.TensorProto.FLOAT,
+            [None, 3, 10, 10],
+        )
         model_output_name = "Y"
-        Y = onnx.helper.make_tensor_value_info(model_output_name,
-                                            onnx.TensorProto.FLOAT,
-                                            [ None,3, 3, 10])
+        Y = onnx.helper.make_tensor_value_info(
+            model_output_name,
+            onnx.TensorProto.FLOAT,
+            [None, 3, 3, 10],
+        )
 
-        indice_name = 'indice'
-        indice = np.random.randint(0,10,(3))
-        indice_initializer = acetoneTestCase.create_initializer_tensor(name = indice_name,
-                                                                       tensor_array = indice,
-                                                                       data_type = onnx.TensorProto.INT32)
-        
+        indice_name = "indice"
+        indice = np.random.randint(0, 10, (3))
+        indice_initializer = acetoneTestCase.create_initializer_tensor(
+            name=indice_name,
+            tensor_array=indice,
+            data_type=onnx.TensorProto.INT32,
+        )
+
         gather_node = onnx.helper.make_node(
-            name = 'Gather',
-            op_type = 'Gather',
-            inputs = [model_input_name,indice_name],
-            outputs = [model_output_name],
-            axis = 2,
+            name="Gather",
+            op_type="Gather",
+            inputs=[model_input_name, indice_name],
+            outputs=[model_output_name],
+            axis=2,
         )
         graph = onnx.helper.make_graph(
-            nodes = [gather_node],
-            name = 'Gather',
-            inputs = [X],
-            outputs = [Y],
-            initializer = [indice_initializer],
+            nodes=[gather_node],
+            name="Gather",
+            inputs=[X],
+            outputs=[Y],
+            initializer=[indice_initializer],
         )
         model = onnx.helper.make_model(graph)
         model = onnx.shape_inference.infer_shapes(model)
         onnx.checker.check_model(model)
-        dataset = acetoneTestCase.create_dataset(self.tmpdir_name,testshape)
-        onnx.save(model,self.tmpdir_name+'/model.onnx' )
+        dataset = acetoneTestCase.create_dataset(self.tmpdir_name, testshape)
+        onnx.save(model, self.tmpdir_name + "/model.onnx")
 
-        sess = rt.InferenceSession(self.tmpdir_name+'/model.onnx')
+        sess = rt.InferenceSession(self.tmpdir_name + "/model.onnx")
         input_name = sess.get_inputs()[0].name
-        result = sess.run(None,{input_name: dataset[0]})
+        result = sess.run(None, {input_name: dataset[0]})
         onnx_result = result[0].ravel().flatten()
-        acetone_result = acetoneTestCase.run_acetone_for_test(self.tmpdir_name,self.tmpdir_name+'/model.onnx', self.tmpdir_name+'/dataset.txt')
-        self.assertListAlmostEqual(list(acetone_result[1]),onnx_result)
-    
+        acetone_result = acetoneTestCase.run_acetone_for_test(
+            self.tmpdir_name,
+            self.tmpdir_name + "/model.onnx",
+            self.tmpdir_name + "/dataset.txt",
+        )
+        self.assertListAlmostEqual(list(acetone_result[1]), onnx_result)
 
     def testGather2(self):
-        testshape = (1,3,10,10)
+        testshape = (1, 3, 10, 10)
 
         model_input_name = "X"
-        X = onnx.helper.make_tensor_value_info(model_input_name,
-                                            onnx.TensorProto.FLOAT,
-                                            [ None,3, 10, 10])
+        X = onnx.helper.make_tensor_value_info(
+            model_input_name,
+            onnx.TensorProto.FLOAT,
+            [None, 3, 10, 10],
+        )
         model_output_name = "Y"
-        Y = onnx.helper.make_tensor_value_info(model_output_name,
-                                            onnx.TensorProto.FLOAT,
-                                            [ None,3, 10, 3])
+        Y = onnx.helper.make_tensor_value_info(
+            model_output_name,
+            onnx.TensorProto.FLOAT,
+            [None, 3, 10, 3],
+        )
 
-        indice_name = 'indice'
-        indice = np.random.randint(0,10,(3))
-        indice_initializer = acetoneTestCase.create_initializer_tensor(name = indice_name,
-                                                                       tensor_array = indice,
-                                                                       data_type = onnx.TensorProto.INT32)
-        
+        indice_name = "indice"
+        indice = np.random.randint(0, 10, (3))
+        indice_initializer = acetoneTestCase.create_initializer_tensor(
+            name=indice_name,
+            tensor_array=indice,
+            data_type=onnx.TensorProto.INT32,
+        )
+
         gather_node = onnx.helper.make_node(
-            name = 'Gather',
-            op_type = 'Gather',
-            inputs = [model_input_name,indice_name],
-            outputs = [model_output_name],
-            axis = 3,
+            name="Gather",
+            op_type="Gather",
+            inputs=[model_input_name, indice_name],
+            outputs=[model_output_name],
+            axis=3,
         )
         graph = onnx.helper.make_graph(
-            nodes = [gather_node],
-            name = 'Gather',
-            inputs = [X],
-            outputs = [Y],
-            initializer = [indice_initializer],
+            nodes=[gather_node],
+            name="Gather",
+            inputs=[X],
+            outputs=[Y],
+            initializer=[indice_initializer],
         )
         model = onnx.helper.make_model(graph)
         model = onnx.shape_inference.infer_shapes(model)
         onnx.checker.check_model(model)
-        dataset = acetoneTestCase.create_dataset(self.tmpdir_name,testshape)
-        onnx.save(model,self.tmpdir_name+'/model.onnx' )
+        dataset = acetoneTestCase.create_dataset(self.tmpdir_name, testshape)
+        onnx.save(model, self.tmpdir_name + "/model.onnx")
 
-        sess = rt.InferenceSession(self.tmpdir_name+'/model.onnx')
+        sess = rt.InferenceSession(self.tmpdir_name + "/model.onnx")
         input_name = sess.get_inputs()[0].name
-        result = sess.run(None,{input_name: dataset[0]})
+        result = sess.run(None, {input_name: dataset[0]})
         onnx_result = result[0].ravel().flatten()
-        acetone_result = acetoneTestCase.run_acetone_for_test(self.tmpdir_name,self.tmpdir_name+'/model.onnx', self.tmpdir_name+'/dataset.txt')
-        self.assertListAlmostEqual(list(acetone_result[1]),onnx_result)
+        acetone_result = acetoneTestCase.run_acetone_for_test(
+            self.tmpdir_name,
+            self.tmpdir_name + "/model.onnx",
+            self.tmpdir_name + "/dataset.txt",
+        )
+        self.assertListAlmostEqual(list(acetone_result[1]), onnx_result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     acetoneTestCase.main()
