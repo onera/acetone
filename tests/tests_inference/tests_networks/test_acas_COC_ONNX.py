a9f63342707e2bf3895d24f0a6a56a3298e001fd
Benjamin Lesage <m.aqpl.c@gmail.com>
2024-10-17 08:43:33 +0200

    Compare ACAS ONNX run to Acetone Python-based inference

--- tests/tests_inference/tests_networks/test_acas_COC_ONNX.py
+++ tests/tests_inference/tests_networks/test_acas_COC_ONNX.py
@@ -50,6 +50,28 @@ class TestAcasCOCONNX(acetoneTestCase.AcetoneTestCase):
         )
         self.assertListAlmostEqual(acetone_result[0], onnx_result)
 
+    def test_acas_coc_onnx_python_ref(self) -> None:
+        """Tests Acas COC, ONNX model, compare between onnx et C code."""
+        model_path = MODELS_DIR / "acas" / "acas_COC" / "nn_acas_COC.onnx"
+        model = onnx.load(model_path)
+        testshape = tuple(
+            model.graph.input[0].type.tensor_type.shape.dim[i].dim_value
+            for i in range(len(model.graph.input[0].type.tensor_type.shape.dim))
+        )
+        dataset = acetoneTestCase.create_dataset(self.tmpdir_name, testshape)
+
+        sess = rt.InferenceSession(model_path)
+        input_name = sess.get_inputs()[0].name
+        result = sess.run(None, {input_name: dataset[0]})
+        onnx_result = result[0].ravel().flatten()
+        acetone_result = acetoneTestCase.run_acetone_for_test(
+            self.tmpdir_name,
+            model_path,
+            self.tmpdir_name + "/dataset.txt",
+            run_generated=False,
+            )
+        self.assertListAlmostEqual(acetone_result[1], onnx_result)
+
     def test_acas_coc_onnx_python(self) -> None:
         """Tests Acas COC, ONNX model, compare between python et C code."""
         model_path = MODELS_DIR / "acas" / "acas_COC" / "nn_acas_COC.onnx"
