5e0a006777ed0143a0f43b4b92504f831bf328ca
Benjamin Lesage <m.aqpl.c@gmail.com>
2025-07-24 14:22:08 +0200

    Update test to support constant quantization

--- tests/tests_inference/tests_networks/test_Qacas_COC_ONNX.py
+++ tests/tests_inference/tests_networks/test_Qacas_COC_ONNX.py
@@ -16,11 +16,11 @@
 * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 ******************************************************************************
 """
+
 from tests.common import MODELS_DIR
 from tests.tests_inference import acetoneTestCase
-import unittest
 
-target_conf='''
+target_conf = """
 {
     "name":"AVX",
     "cflags":"-O1",
@@ -32,89 +32,129 @@ target_conf='''
         "temp_pydtype":"int32",
         "layers":
         {
-            "Input_layer_0":{
+            "X":{
                 "out":"Q0.15"
             },
-            "MatMul_1":{
+            "W0":{
+                "out":"Q-3.18"
+            },
+            "MatMul_13":{
                 "in":"Q0.15",
                 "params":"Q-3.18",
                 "out":"Q2.13"
             },
-            "Add_2":{
+            "B0": {
+                "out":"Q0.15"
+            },
+            "Add_14":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "MatMul_3":{
+            "W1": {
+                "out":"Q0.15"
+            },
+            "MatMul_15":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "Add_4":{
+            "B1": {
+                "out":"Q0.15"
+            },
+            "Add_16":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "MatMul_5":{
+            "W2": {
+                "out":"Q0.15"
+            },
+            "MatMul_17":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "Add_6":{
+            "B2": {
+                "out":"Q0.15"
+            },
+            "Add_18":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "MatMul_7":{
+            "W3": {
+                "out":"Q0.15"
+            },
+            "MatMul_19":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "Add_8":{
+            "B3": {
+                "out":"Q0.15"
+            },
+            "Add_20":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "MatMul_9":{
+            "W4": {
+                "out":"Q0.15"
+            },
+            "MatMul_21":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "Add_10":{
+            "B4": {
+                "out":"Q0.15"
+            },
+            "Add_22":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "MatMul_11":{
+            "W5": {
+                "out":"Q0.15"
+            },
+            "MatMul_23":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             },
-            "Add_12":{
+            "B5": {
+                "out":"Q0.15"
+            },
+            "Add_24":{
                 "in":"Q0.15",
                 "params":"Q0.15",
                 "out":"Q2.13"
             }
         }
-    }  
+    }
 }
-'''
+"""
+
+
 def writeconf(conf):
-    with open('AVX512VNNI.json','w') as f:
+    with open("AVX512VNNI.json", "w") as f:
         f.write(conf)
+
 
 class TestQAcasCOCONNX(acetoneTestCase.AcetoneTestCase):
     """Inference test for Quantized ACAS COC, ONNX model."""
+
     def test_Qacas_coc_onnx_python(self) -> None:
         """Tests Acas COC, ONNX model, compare between python et C code."""
         model_path = MODELS_DIR / "acas" / "acas_COC" / "nn_acas_COC.onnx"
 
-        writeconf(target_conf) # writes target config AVX512VNNI.json in the CWD
+        writeconf(target_conf)  # writes target config AVX512VNNI.json in the CWD
 
         c_result, py_result = acetoneTestCase.run_acetone_for_test(
             self.tmpdir_name,
             model_path,
-            target='AVX512VNNI'
+            target="AVX512VNNI",
         )
 
         self.assertListAlmostEqual(c_result, py_result)
