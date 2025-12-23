aed0260b44b29641d90b69975c1bc0d13e61e523
valot <nicolas.valot@airbus.com>
2025-12-23 16:59:07 +0100

    reset18 pytorch with bounded activation OK

--- src/acetone_nnet/importers/PYTORCH_importer/parse_pytorch.py
+++ src/acetone_nnet/importers/PYTORCH_importer/parse_pytorch.py
@@ -5,6 +5,7 @@ import numpy as np
 from acetone_nnet.generator import (
     ActivationFunctions,
     Add,
+    Clip,
     Average,
     AveragePooling2D,
     BatchNormalization,
@@ -146,11 +147,14 @@ class ShapeAwareVisitor(FXGraphVisitor):
             self.layerdic[node.name] = (this_layer, node.meta['val'].shape)
             self.idx+=1
         elif "relu" in target_name:
-            self.layerdic[node.args[0].name][0].activation_function=ReLu()
-            self.layerdic[node.name] = self.layerdic[node.args[0].name]
+            self.layerdic[node.args[0].name][0].activation_function=ReLu() # set previous layer activation 
+            self.layerdic[node.name] = self.layerdic[node.args[0].name] # register 
         elif "clamp" in target_name:
-            self.layerdic[node.args[0].name][0].activation_function=ReLu()
-            self.layerdic[node.name] = self.layerdic[node.args[0].name]
+            self.layerdic[node.args[0].name][0].activation_function=Clip( # set previous layer activation 
+                max_value=node.args[2],
+                min_value=node.args[1]
+            )
+            self.layerdic[node.name] = self.layerdic[node.args[0].name] # register 
         elif "conv2d" in target_name:
             stride = node.args[3][0] if len(node.args) > 3 else 1
             padding = node.args[4] + node.args[4] if len(node.args) > 4 else [0,0,0,0]
@@ -169,13 +173,45 @@ class ShapeAwareVisitor(FXGraphVisitor):
                     input_shape=self.layerdic[node.args[0].name][1],
                     output_shape=node.meta['val'].shape,
                     weights=self.module.state_dict()[node.args[1].target].numpy(),
-                    biases=self.module.state_dict()[node.args[2].target].numpy(),
+                    biases=self.module.state_dict()[node.args[2].target].numpy() if node.args[2] is not None else np.zeros(node.meta['val'].shape[1],dtype=np.float32),
                     activation_function=Linear(),
                 )
             self.layerdic[node.args[0].name][0].next_layer.append(this_layer)
             this_layer.previous_layer.append(self.layerdic[node.args[0].name][0])
             self.layerdic[node.name] = (this_layer, node.meta['val'].shape)
             self.idx+=1
+        elif "batch_norm" in target_name:
+            # Fusing batch normalization to previous conv2d https://pytorch-cn.com/tutorials/intermediate/fx_conv_bn_fuser.html
+            self.layerdic[node.name] = self.layerdic[node.args[0].name]
+            bn_w = self.module.state_dict()[node.args[1].target]
+            bn_b = self.module.state_dict()[node.args[2].target]
+            bn_rm = self.module.state_dict()[node.args[3].target]
+            bn_rv = self.module.state_dict()[node.args[4].target]
+            bn_eps = node.args[7]
+            bn_var_rsqrt = torch.rsqrt(bn_rv + bn_eps)
+            conv_w = torch.from_numpy(self.layerdic[node.args[0].name][0].weights)
+            conv_b = torch.from_numpy(self.layerdic[node.args[0].name][0].biases)
+            conv_w = conv_w * (bn_w * bn_var_rsqrt).reshape([-1] + [1] * (len(conv_w.shape) - 1))
+            conv_b = (conv_b - bn_rm) * bn_var_rsqrt * bn_w + bn_b
+            self.layerdic[node.args[0].name][0].weights = conv_w.numpy().astype(np.float32)
+            self.layerdic[node.args[0].name][0].biases = conv_b.numpy().astype(np.float32)
+        elif "add" in target_name:
+            if node.args[0].name in self.layerdic.keys(): # bypass bn2_num_batches_tracked + 1
+                this_layer =  Add(
+                    original_name=node.name,
+                    idx=self.idx,
+                    size=node.meta['val'].numel(),
+                    input_shapes=[self.layerdic[node.args[i].name][1] for i in range(len(node.args))],
+                    output_shape=node.meta['val'].shape,
+                    activation_function=Linear(),
+                )
+                for i in range(len(node.args)):
+                    self.layerdic[node.args[i].name][0].next_layer.append(this_layer)
+                    this_layer.previous_layer.append(self.layerdic[node.args[i].name][0])
+                self.layerdic[node.name] = (this_layer, node.meta['val'].shape)
+                self.idx+=1
+            else:
+                logging.info(f"Add layer bypassed: {node.args[0].name}")
         elif "cat" in target_name:
             this_layer = Concatenate(
                     original_name=node.name,
@@ -192,6 +228,8 @@ class ShapeAwareVisitor(FXGraphVisitor):
             self.layerdic[node.name] = (this_layer, node.meta['val'].shape)
             self.idx+=1
         elif "dropout" in target_name:
+            self.layerdic[node.name] = self.layerdic[node.args[0].name]
+        elif "flatten" in target_name:
             self.layerdic[node.name] = self.layerdic[node.args[0].name]
         elif "max_pool2d" in target_name:
             kernel = node.args[1][0] if len(node.args) > 1 else 1
